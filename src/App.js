f6702a43e85b1f4b1fb480df63e2f0f91278aa41
Candelaojeda <ojedacandelaco@gmail.com>
2021-07-19 17:33:08 -0300

    modified files and add folder components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component() {
+  render(){
+    return(
+      <div>
+        <Router>
+          <Link to="/">Home | </Link>
+          <Link to="/books">Books</Link>
+          <Route exact path="/" render={() => {
+            return(
+              <div>
+
+              </div>
+            )
+          }}>
+
+          </Route>
+          <Route path="/books" component={Books} />
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
